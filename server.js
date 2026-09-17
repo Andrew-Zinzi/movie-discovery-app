eb1ed3e6f10f81b8e023439d681adea297e16366
Mug-Maniac <andrewzinzi@gmail.com>
2026-09-16 21:46:52 -0400

    Fixed minor bug/syntactical error, put request function was inside post request function, moved put outside

--- server.js
+++ server.js
@@ -120,23 +120,23 @@ app.post("/favorites", (req, res) => {
       rating: 0,
     };
 
-    app.put("/favorites/:id", (req, res) => {
-      const favorite = favorites.find(
-        (favorite) => favorite.id.toString() === req.params.id,
-      );
-
-      if (!favorite) {
-        return res.status(404).json({
-          error: "404 not found: The requested favorite ID does not exist",
-        });
-      } else {
-        favorite.rating = req.body.rating;
-        return res.status(200).json(favorite);
-      }
-    });
-
     favorites.push(newFavorite);
     return res.status(201).json(newFavorite);
+  }
+});
+
+app.put("/favorites/:id", (req, res) => {
+  const favorite = favorites.find(
+    (favorite) => favorite.id.toString() === req.params.id,
+  );
+
+  if (!favorite) {
+    return res.status(404).json({
+      error: "404 not found: The requested favorite ID does not exist",
+    });
+  } else {
+    favorite.rating = req.body.rating;
+    return res.status(200).json(favorite);
   }
 });
 
