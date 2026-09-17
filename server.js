9ecd9c73d59d1855e7248eb89fe2ee9ca85217d8
Mug-Maniac <andrewzinzi@gmail.com>
2026-09-16 22:16:13 -0400

    feat: added initial logic, get favorite based on id, splice favorites based on index of that favoirte object

--- server.js
+++ server.js
@@ -140,6 +140,21 @@ app.put("/favorites/:id", (req, res) => {
   }
 });
 
+app.delete("/favorites/:id", (req, res) => {
+  const favorite = favorites.find(
+    (favorite) => favorite.id.toString() === req.params.id,
+  );
+
+  if (!favorite) {
+    return res.status(404).json({
+      error: "404 not found: The requested favorite ID does not exist",
+    });
+  } else {
+    favorites.splice(favorites.indexOf(favorite), 1);
+    return res.status(200).json(favorites);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`listening on port: ${PORT}`);
 });
